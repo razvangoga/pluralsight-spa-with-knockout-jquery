6dd4d69143cee414b3efeb992c1750a992d44813
razvan goga <razvan.goga@gmail.com>
2012-10-04 14:13:19 +0300

    Added model entities

---
+++ CodeCamper.Model/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeCamper.Model
+{
+    public class Session : SessionBrief
+    {
+        public string Description { get; set; }
+        
+        public Person Speaker { get; set; }
+        public virtual Track Track { get; set; }
+        public virtual TimeSlot TimeSlot { get; set; }
+        public virtual Room Room { get; set; }
+
+        public virtual ICollection<Attendance> AttendanceList { get; set; }
+
+    }
+}
