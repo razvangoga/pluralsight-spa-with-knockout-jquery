e5f7e05a64ab4d00ef9c0adf60211ed4a3b8646f
razvan goga <razvan.goga@gmail.com>
2012-10-04 14:39:10 +0300

    created EF DbContext, Db Context Configs, Seed data

---
+++ CodeCamper.Data/CodeCamperDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using CodeCamper.Model;
+using CodeCamper.Data.SampleData;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CodeCamper.Data
+{
+    public class CodeCamperDbContext : DbContext
+    {
+        public CodeCamperDbContext() : base(nameOrConnectionString: "CodeCamper") { }
+
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Attendance> Attendance { get; set; }
+
+        //lookups
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+        static CodeCamperDbContext()
+        {
+            Database.SetInitializer(new CodeCamperDatabaseInitializer());
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new SessionConfiguration());
+            modelBuilder.Configurations.Add(new AttendanceConfiguration());
+        }
+    }
+}
